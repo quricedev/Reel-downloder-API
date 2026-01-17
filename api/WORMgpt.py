3152abccc1450528de32dd7eed0e4ceec91f37a4
quricedev <pvishwas382@gmail.com>
2026-01-18 00:01:58 +0530

    Fix OpenAI client initialization parameter name

--- api/WORMgpt.py
+++ api/WORMgpt.py
@@ -8,7 +8,7 @@ from openai import OpenAI
 
 client = OpenAI(
     api_key=os.environ.get("DEEPSEEK_API_KEY"),
-    OPENAI_BASE_URL=os.environ.get("OPENAI_BASE_URL")
+    base_url=os.environ.get("OPENAI_BASE_URL")
 )
 
 MODEL_NAME = os.environ.get("DEEPSEEK_MODEL")
